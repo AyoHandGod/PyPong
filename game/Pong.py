220904b48bc418ef916478518508897027531c3f
Dayo <Calikidd86@gmail.com>
2019-07-05 00:42:38 -0700

    Initial Commit

--- game/Pong.py
+++ game/Pong.py
@@ -1,3 +1,8 @@
+"""
+Pong Game implemented using Python and Kivy.
+
+"""
+
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.uix.widget import Widget
@@ -11,25 +16,21 @@ from kivy.config import Config
 
 from random import randint
 
-
-Config.set('graphics', 'width', '800')
-Config.set('graphics', 'height', '800')
-
-
-
-class PongPaddle(Widget):
-    score = NumericProperty(0)
-
-    def bounce_ball(self, ball):
-        if self.collide_widget(ball):
-            vx, vy = ball.velocity
-            offset = (ball.center_y - self.center_y) / (self.height / 2)
-            bounced = Vector(-1 * vx, vy)
-            vel = bounced * 1.1
-            ball.velocity = vel.x, vel.y + offset
+Config.set('graphics', 'width', '200')
+Config.set('graphics', 'height', '200')
+Config.set('kivy', 'window_icon', 'successLogo.png')
 
 
 class PongBall(Widget):
+    """
+    This class represents the Ball.
+
+    Attributes:
+        velocity_x (NumericProperty): represents the velocity of the ball along the x axis.
+        velocity_y (NumericProperty): represents the velocity of the ball along the y axis.
+        velocity (ReferenceListProperty): allows shorthand for velocity_x/velocity_y i.e. velocity.x.
+
+    """
     # velocity of the ball on x and y axis
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
@@ -40,32 +41,74 @@ class PongBall(Widget):
     # ''move'' function will move the ball on one step. THis
     # will be called in equal intervals to animate movement
     def move(self):
+        """
+        This method moves the ball using velocity.
+        """
         self.pos = Vector(*self.velocity) + self.pos
 
 
+class PongPaddle(Widget):
+    """
+    This class is used to represent the Paddle and players functionality.
+
+    PongPaddle object is used to provide the players behavior and maintain score state.
+
+    Attributes:
+        score (NumericProperty):  The current score of the player controlling this paddle.
+
+    """
+    score = NumericProperty(0)
+
+    def bounce_ball(self, ball: PongBall):
+        """
+        The method used to bounce the ball off of paddle upon collision.
+        Args:
+            ball (PongBall):  The ball to be bounced.
+        """
+        if self.collide_widget(ball):
+            vx, vy = ball.velocity
+            offset = (ball.center_y - self.center_y) / (self.height / 2)
+            bounced = Vector(-1 * vx, vy)
+            vel = bounced * 1.1
+            ball.velocity = vel.x, vel.y + offset
+
+
 class PongGame(Widget):
-    # ObjectProperty for PongBall, will link with id property
+    """
+    This widget is used to represent the PongGame as a whole.
+
+    Attributes:
+        ball (ObjectProperty): Will hold the id of the PongBall associated with this PongGame.
+        player1 (ObjectProperty): Will hold the id of the PongPaddle associated with player1.
+        player2 (ObjectProperty): Will hold the id of the PongPaddle associated with player2.
+    """
     ball = ObjectProperty(None)
     player1 = ObjectProperty(None)
     player2 = ObjectProperty(None)
 
-    def serve_ball(self, vel=(4, 0)):
+    def serve_ball(self, vel: Vector = (4, 0)):
+        """
+        This method sets the ball object in motion.
+        Args:
+            vel (Tuple): the velocity to be applied to the ball.
+        """
         self.ball.center = self.center
         self.ball.velocity = vel
 
     def update(self, dt):
-        # call ball.move and other stuff
+        """
+        This method handles updating the game state each frame
+        Args:
+            dt: kivy required param provided by default
+        """
         self.ball.move()
 
-        # bounce of paddles
         self.player1.bounce_ball(self.ball)
         self.player2.bounce_ball(self.ball)
 
-        # bounce off top and bottom
         if (self.ball.y < 0) or (self.ball.top > self.height):
             self.ball.velocity_y *= -1
 
-        # bounce off left and right
         if self.ball.x < self.x:
             self.player2.score += 1
             self.serve_ball(vel=(4, 0))
@@ -75,16 +118,28 @@ class PongGame(Widget):
             self.serve_ball(vel=(-4, 0))
 
     def on_touch_move(self, touch):
+        """
+        This method handles the input behavior for our game, moving the paddles.
+
+        Which of the paddles moved is determined based on the x coordinate of the input.
+        The y coordinate of the paddle is
+        Args:
+            touch (Input): the touch/mouse input on screen.
+        """
         if touch.x < self.width / 3:
             self.player1.center_y = touch.y
         if touch.x > self.width - self.width / 3:
             self.player2.center_y = touch.y
 
+
 class PongApp(App):
+    """Pong Game GUI Application"""
+
     def build(self):
         game = PongGame()
         game.serve_ball()
-        Clock.schedule_interval(game.update, 1.0 / 60.0)
+        self.icon = 'game/successLogo.png'
+        Clock.schedule_interval(game.update, 1.0 / 60.0) # makes the game update at 60 frames per sec
         return game
 
 
